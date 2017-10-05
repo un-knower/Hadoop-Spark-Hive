5d479d5e3db0480dcf1dfe79a3f73d7968f3bdbb
baolibin <2578858653@qq.com>
2017-10-05 22:32:37 +0800

    hello

--- src/main/com/libin/spark/hello.scala
+++ src/main/com/libin/spark/hello.scala
@@ -1,7 +1,13 @@
 package libin.spark
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object hello {
   def main(args: Array[String]): Unit = {
-    println("hello spark")
+
+    val sc=new SparkContext(new SparkConf().setAppName("hello").setMaster("local"))
+    val trainData = sc.parallelize(Array(3,5,7))
+
+    trainData.collect().foreach(println)
   }
 }
